48ad3d3fdfed12f0bc955e88ceb45a59b8693d3f
sogaiu <983021772@users.noreply.github.com>
2022-02-11 20:24:50 +0900

    Improve unix desktop build script

--- build-desktop.sh
+++ build-desktop.sh
@@ -1,31 +1,40 @@
 #! /bin/sh
 
-# XXX: make janet once before proceeding, e.g.
-#
-#      cd janet && make clean && PREFIX=resources make
-#
-#      this should produce a build subdirectory in janet with some bits
+# exit if some part fails
+set -e
 
-# XXX: make libraylib.a also, e.g.
-#
-#      make clean
-#      cd jaylib/raylib/src
-#      make
-#
-#      this should produce libraylib.a in jaylib/raylib
+if [[ -z "${JAYLIB_WASM_DEMO_SKIP_DEPS}" ]]; then
+    echo "[Preparing janet amalgamated bits]..."
+    cd janet && \
+        make clean && \
+        PREFIX=resources make && \
+        cd ..
 
-# XXX: make jaylib.janet shim
-#
-#      janet make-jaylib-janet-shim.janet jaylib/src resources/lib/janet/jaylib.janet
+    echo "[Preparing libraylib.a]..."
+    cd jaylib/raylib/src && \
+        make clean && \
+        make &&
+        cd ../../..
 
-    #-O2 \
-# XXX: only tested on linux
+    echo "[Preparing jaylib.janet shim]..."
+    mkdir -p resources/lib/janet && \
+        janet make-jaylib-janet-shim.janet jaylib/src resources/lib/janet/jaylib.janet
+fi
+
+[ -e janet/build/c/janet.c ] || \
+    (echo "janet/build/c/janet.c not found, please build" && exit 1)
+
+[ -e jaylib/raylib/libraylib.a ] || \
+    (echo "jaylib/raylib/libraylib.a not found, please build" && exit 1)
+
+echo "[Compiling output]..."
 gcc \
     -O0 -g3 \
     -Wall \
     -o main \
     main.c \
-    janet/build/c/janet.c jaylib/raylib/libraylib.a \
+    janet/build/c/janet.c \
+    jaylib/raylib/libraylib.a \
     -Ijanet/build \
     -Ijaylib/src \
     -Ijaylib/raylib/src \
